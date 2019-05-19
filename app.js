aa2599b85b808c23a0478a6eb31c60e4ead53171
skripakoksana <50514474+skripakoksana@users.noreply.github.com>
2019-05-19 16:52:45 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+
+//var discount = 0.2
+//var currency = "грн"
+
+
+//function calcDiscount(productNumber){
+//var productPrice = prompt("введіть вартість товару","")
+//var productPriceWithDiscount = productPrice - (productPrice*discount)
+
+//console.log ("Вартість" + productNumber +" товару зі знижкою"  + productPriceWithDiscount + " " + currency)
+//}
+
+//calcDiscount(2)
+//calcDiscount(10)
+//calcDiscount(100)
+
+
+//function sum(a,b){
+//	return a+b
+//}
+//console.log(sum(5,5))
+//console.log(sum(10,5))
+
+//function sum(a,b){
+//console.log(a+b)
+//}
+//sum(5,5)
+//sum(10,5)
+
+
+var discount = 0.2
+var currency = "грн"
+var productNumber = 1;
+
+function calcDiscount{
+var productPrice = prompt("введіть вартість товару","")
+var productPriceWithDiscount = productPrice - (productPrice*discount)
+
+console.log ("Вартість" + productNumber++ +" товару зі знижкою"  + productPriceWithDiscount + " " + currency)
+}
+
+calcDiscount()
+calcDiscount()
+calcDiscount()
